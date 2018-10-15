086c8c6586a4fe5d096954304bd944a13dcb9339
David Petticrew <cardassianscot@gmail.com>
2018-10-15 13:19:02 +0100

    Setup

--- HidingStuff/Form1.cs
+++ HidingStuff/Form1.cs
@@ -16,5 +16,43 @@ namespace HidingStuff
         {
             InitializeComponent();
         }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (comboBox1.SelectedIndex == 0)
+            {
+                label1.Show();
+                label2.Hide();
+                label3.Hide();
+                label4.Hide();
+                label5.Hide();
+                label6.Hide();
+                label7.Hide();
+            }
+            else if (comboBox1.SelectedIndex == 1)
+            {
+
+            }
+            else if (comboBox1.SelectedIndex == 2)
+            {
+
+            }
+            else if (comboBox1.SelectedIndex == 3)
+            {
+
+            }
+            else if (comboBox1.SelectedIndex == 4)
+            {
+
+            }
+            else if (comboBox1.SelectedIndex == 5)
+            {
+
+            }
+            else if (comboBox1.SelectedIndex == 6)
+            {
+
+            }
+        }
     }
 }
