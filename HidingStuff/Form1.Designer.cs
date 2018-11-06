72e8a94cc93a8688f01ef6fc5620667a52949e56
David Petticrew <cardassianscot@gmail.com>
2018-11-06 07:46:41 +0000

    Refactored for HideAll

--- HidingStuff/Form1.Designer.cs
+++ HidingStuff/Form1.Designer.cs
@@ -41,68 +41,69 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(131, 189);
+            this.label1.Location = new System.Drawing.Point(150, 202);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(79, 29);
+            this.label1.Size = new System.Drawing.Size(93, 32);
             this.label1.TabIndex = 0;
             this.label1.Text = "label1";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(131, 256);
+            this.label2.Location = new System.Drawing.Point(150, 274);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(79, 29);
+            this.label2.Size = new System.Drawing.Size(93, 32);
             this.label2.TabIndex = 1;
             this.label2.Text = "label2";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(131, 315);
+            this.label3.Location = new System.Drawing.Point(150, 337);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(79, 29);
+            this.label3.Size = new System.Drawing.Size(93, 32);
             this.label3.TabIndex = 2;
             this.label3.Text = "label3";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(131, 398);
+            this.label4.Location = new System.Drawing.Point(150, 425);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(79, 29);
+            this.label4.Size = new System.Drawing.Size(93, 32);
             this.label4.TabIndex = 3;
             this.label4.Text = "label4";
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(131, 463);
+            this.label5.Location = new System.Drawing.Point(150, 495);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(79, 29);
+            this.label5.Size = new System.Drawing.Size(93, 32);
             this.label5.TabIndex = 4;
             this.label5.Text = "label5";
             // 
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(131, 506);
+            this.label6.Location = new System.Drawing.Point(150, 541);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(79, 29);
+            this.label6.Size = new System.Drawing.Size(93, 32);
             this.label6.TabIndex = 5;
             this.label6.Text = "label6";
             // 
             // label7
             // 
             this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(131, 566);
+            this.label7.Location = new System.Drawing.Point(150, 605);
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(79, 29);
+            this.label7.Size = new System.Drawing.Size(93, 32);
             this.label7.TabIndex = 6;
             this.label7.Text = "label7";
             // 
             // comboBox1
             // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.comboBox1.FormattingEnabled = true;
             this.comboBox1.Items.AddRange(new object[] {
             "Label 1",
@@ -112,17 +113,17 @@
             "Label 5",
             "Label 6",
             "Label 7"});
-            this.comboBox1.Location = new System.Drawing.Point(113, 81);
+            this.comboBox1.Location = new System.Drawing.Point(129, 87);
             this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(219, 37);
+            this.comboBox1.Size = new System.Drawing.Size(250, 39);
             this.comboBox1.TabIndex = 7;
             this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(14F, 29F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 31F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1326, 832);
+            this.ClientSize = new System.Drawing.Size(1515, 889);
             this.Controls.Add(this.comboBox1);
             this.Controls.Add(this.label7);
             this.Controls.Add(this.label6);
--- HidingStuff/Form1.cs
+++ HidingStuff/Form1.cs
@@ -19,40 +19,46 @@ namespace HidingStuff
 
         private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
         {
+            HideAll();
             if (comboBox1.SelectedIndex == 0)
             {
                 label1.Show();
-                label2.Hide();
-                label3.Hide();
-                label4.Hide();
-                label5.Hide();
-                label6.Hide();
-                label7.Hide();
             }
             else if (comboBox1.SelectedIndex == 1)
             {
-
+                label2.Show();
             }
             else if (comboBox1.SelectedIndex == 2)
             {
-
+                label3.Show();
             }
             else if (comboBox1.SelectedIndex == 3)
             {
-
+                label4.Show();
             }
             else if (comboBox1.SelectedIndex == 4)
             {
-
+                label5.Show();
             }
             else if (comboBox1.SelectedIndex == 5)
             {
-
+                label6.Show();
             }
             else if (comboBox1.SelectedIndex == 6)
             {
-
+                label7.Show();
             }
+        }
+
+        private void HideAll()
+        {
+            label1.Hide();
+            label2.Hide();
+            label3.Hide();
+            label4.Hide();
+            label5.Hide();
+            label6.Hide();
+            label7.Hide();
         }
     }
 }
